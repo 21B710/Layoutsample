6d6285e7fe4d72f648302e85b4f4b53afeda3e58
unknown <s21b710@shohoku.ac.jp>
2021-11-26 10:38:00 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/shohoku/layoutsample/Gamen1.java
@@ -0,0 +1,13 @@
+package jp.ac.shohoku.layoutsample;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Gamen1 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
